ddffa1f85f8d6028bc5060e9e83d77437ca2ef0e
Oliver Xu <olxu@hotmail.com>
2021-05-19 23:07:37 +1000

    add helper functions

--- top_cessna_models.py
+++ top_cessna_models.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from sparkmeasure import StageMetrics
 stagemetrics = StageMetrics(spark)
+from utils import clean_column_names, evaluate_metrics
 import re
 
 def top_cessna_models(spark_session, flights_path, aircrafts_path):
@@ -18,7 +19,9 @@ def top_cessna_models(spark_session, flights_path, aircrafts_path):
     cessna_flights_count = (cessna_flights.groupBy('model')
                                           .count()
                                           .orderBy('count', ascending=False))
-    cessna_flights_count.show()
+
+    for aircraft in clean_column_names(cessna_flights_count).take(3):
+        print("Cessna " + aircraft.model[0:3] + '\t' + str(aircraft.count))
 
 if __name__ == "__main__":
     spark = (SparkSesssion
@@ -33,13 +36,3 @@ if __name__ == "__main__":
                       f"{dbfs_fileStore_prefix}/{prefix}_flights_{size}.csv", 
                       f"{dbfs_fileStore_prefix}/{prefix}_aircrafts.csv") 
     spark.stop()
-
-
-
-
-
-
-
-
-
-
